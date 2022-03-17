d2eb2aef0c72764e32e75285318c8af4b6f212d0
ferriss122 <czechkamil1@gmail.com>
2022-03-17 23:28:25 +0100

    A program that sums number from 1 to number in function that takes 1 number and measures script performance in another function

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
+import time
+
+
+
 def sum_all_of_the_numbers(number):
     sum = 0
     for number in range(1,number+1):
@@ -5,5 +9,14 @@ def sum_all_of_the_numbers(number):
 
     return sum
 
-n = int(input('Enter a number:'))
-print(sum_all_of_the_numbers(n))
+
+def function_that_measures_script_performance(func,arg):
+    start = time.perf_counter()
+    func(arg)
+    end = time.perf_counter()
+    sumary = end - start
+    return sumary
+
+print(function_that_measures_script_performance(sum_all_of_the_numbers,5000))
+
+
