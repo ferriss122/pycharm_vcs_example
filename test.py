31679ae439bf30f752bfb0c6f887c1c70319acac
ferriss122 <czechkamil1@gmail.com>
2022-03-18 00:15:54 +0100

    A program that asks user what he wants to create either a list or set then ask another question for which number is the user looking for in list or set then return True or False

--- test.py
+++ test.py
@@ -10,13 +10,18 @@ def sum_all_of_the_numbers(number):
     return sum
 
 
-def function_that_measures_script_performance(func,arg):
-    start = time.perf_counter()
-    func(arg)
-    end = time.perf_counter()
-    sumary = end - start
-    return sumary
+def function_that_measures_script_performance(func,arg,how_many_times=2):
+    sum = 0
+    for i in range(0,how_many_times):
+        start = time.perf_counter()
+        func(arg)
+        end = time.perf_counter()
+        sum = sum + (end-start)
+    return sum
 
-print(function_that_measures_script_performance(sum_all_of_the_numbers,5000))
+print(function_that_measures_script_performance(sum_all_of_the_numbers,5000,3))
+print(function_that_measures_script_performance(sum_all_of_the_numbers,10000))
+print(function_that_measures_script_performance(sum_all_of_the_numbers,2500))
+print(function_that_measures_script_performance(sum_all_of_the_numbers,20000))
 
 
