aaf8e48664fdc60657791a24ca0b05ac07984529
ferriss122 <czechkamil1@gmail.com>
2022-03-17 17:58:05 +0100

    testing new function

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+from secret import password
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def my_function(a,b):
+    return a + b
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Kamil')
     print('How are you doing?')
     print('Do you like me or not?')
+    print(password)
+
+    print(my_function(2,4))
 
