602f8b6cedabf4f7fb1d39720283e9c935823ea6
ferriss122 <czechkamil1@gmail.com>
2022-03-23 21:14:18 +0100

    A mini-game where the user have 5 steps and walks through the tunnel by 1 step. Each time you can get nothing or chest that contains gold.You also have menu where you can choose options.You have also chances of getting different amounts of gold from chests.

--- test5.py
+++ test5.py
@@ -1,14 +1,13 @@
-import random,time
+import random
+import time
 
 steps = 5
 
-chest = ['green','orange','purple','gold']
-nothing_or_chest = ['nothing','chest']
-values_of_chests = {'green':1000,'orange':4000,'purple':9000,'gold':16000}
+chest = ['green', 'orange', 'purple', 'gold']
+nothing_or_chest = ['nothing', 'chest']
+values_of_chests = {'green': 1000, 'orange': 4000, 'purple': 9000, 'gold': 16000}
 list_for_gold = []
 list_for_time = []
-
-
 
 Menu_of_your_choices = 0
 while Menu_of_your_choices != 1:
@@ -20,13 +19,13 @@ while Menu_of_your_choices != 1:
     5.sum of your gold
     6.your time
     """))
-    if Menu_of_your_choices ==2:
+    if Menu_of_your_choices == 2:
         print('You have now ', steps, 'steps')
-    elif Menu_of_your_choices ==3:
+    elif Menu_of_your_choices == 3:
         steps = 5
         print('Reset completed!')
-    elif Menu_of_your_choices ==4:
-        while steps>0:
+    elif Menu_of_your_choices == 4:
+        while steps > 0:
             start = time.perf_counter()
 
             print('You have an opportunity to meet nothing or a chest!')
@@ -35,7 +34,7 @@ while Menu_of_your_choices != 1:
             percent_radio_for_nothing_or_chest = random.choices(nothing_or_chest, [40, 60], k=1)
             random_choice = random.choice(percent_radio_for_nothing_or_chest)
             if random_choice == "chest":
-                percent_ratio_for_chest = (random.choices(chest,[75,20,4,1],k=1))
+                percent_ratio_for_chest = (random.choices(chest, [75, 20, 4, 1], k=1))
                 for element in percent_ratio_for_chest:
                     print(element)
                 gold = values_of_chests.get(element)
@@ -43,7 +42,7 @@ while Menu_of_your_choices != 1:
                 print(gold)
             else:
                 print("You meet nothing!")
-            steps-=1
+            steps -= 1
             end = time.perf_counter()
             overall_time = end - start
             list_for_time.append(overall_time)
@@ -60,7 +59,3 @@ while Menu_of_your_choices != 1:
             sum_time = sum_time + time_element
         print('The program overall runned for :')
         print(sum_time)
-
-
-
-
