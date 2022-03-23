23a7eeaeb6ac557d49cadf564e694eae8e2f0f7e
ferriss122 <czechkamil1@gmail.com>
2022-03-23 21:09:36 +0100

    A program that sums numbers from 1 to number defined by user and measures time performance of the program.

--- test3.py
+++ test3.py
@@ -1,9 +1,22 @@
-import random
+import time
 
-steps = 5
+def function_1(number):
+    """Function that sums numbers from 1 to number defined by user."""
+    sum = 0
+    for number in range(1,number+1):
+        sum = sum + number
 
-chest = ['green','orange','purple','legendary']
-nothing_or_chest = ['nothing','chest']
-percent_radio_for_chest = random.choices(chest,[75,20,4,1],k=1)
-percent_radio_for_nothing_or_chest = random.choices(nothing_or_chest,[40,60],k=1)
-print(percent_radio_for_chest)
+    return sum
+
+print(function_1(5))
+print(function_1(10))
+
+def function_2(func,arg):
+    """Function that measures time performance of the script."""
+    start = time.perf_counter()
+    func(arg)
+    end = time.perf_counter()
+    return end - start
+
+print(function_2(function_1,5))
+print(function_2(function_1,10))
