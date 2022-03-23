9532c00860fc29970e227de1f2ff1d1ffb01b124
ferriss122 <101822912+ferriss122@users.noreply.github.com>
2022-03-23 21:08:34 +0100

    Delete test3.py

--- test3.py
+++
@@ -1,9 +0,0 @@
-import random
-
-steps = 5
-
-chest = ['green','orange','purple','legendary']
-nothing_or_chest = ['nothing','chest']
-percent_radio_for_chest = random.choices(chest,[75,20,4,1],k=1)
-percent_radio_for_nothing_or_chest = random.choices(nothing_or_chest,[40,60],k=1)
-print(percent_radio_for_chest)
